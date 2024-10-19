560e9ed28e7a745a80bc4e7c82f4c747f4edb26f
Ahmed Mohamed Salah <112906631+fakhrbasha@users.noreply.github.com>
2024-10-20 01:43:54 +0300

    itii

--- Book_Store/obj/Debug/net8.0/Book_Store.AssemblyInfo.cs
+++ Book_Store/obj/Debug/net8.0/Book_Store.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Book_Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bf311327d27d43cd65d15d8b65a1ca36467ab16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Book_Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Book_Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ITI_Project
+
 ITI_Project Book Store
+
+<img src="/Photo/Store.png" alt="Girl in a jacket">
